23ce4cfadb56516c6498876aca98bc5f3fb2f94d
Elke Van Santvliet <elkevs96@live.be>
2024-12-02 12:57:15 +0100

    Added typing

--- day1/main.py
+++ day1/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
+
 def main():
     list1 = []
     list2 = []
-    with open("day1/input.txt") as lists:
+    with open("day1/example.txt") as lists:
         for loc in lists:
             loc1, loc2 = loc.split()
             list1.append(int(loc1))
@@ -11,13 +12,16 @@ def main():
     print(distance(list1, list2))
     print(compare(list1, list2))
 
-def distance(list1, list2):
-    difs = [abs(loc1-loc2) for loc1, loc2 in zip(sorted(list1), sorted(list2))]
+
+def distance(list1: list[int], list2: list[int]) -> int:
+    difs = [abs(loc1 - loc2) for loc1, loc2 in zip(sorted(list1), sorted(list2))]
     return sum(difs)
 
-def compare(list1, list2):
-    similarity = [loc*list2.count(loc) for loc in list1]
+
+def compare(list1: list[int], list2: list[int]) -> int:
+    similarity = [loc * list2.count(loc) for loc in list1]
     return sum(similarity)
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     main()
