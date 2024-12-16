a563e13588350b1cf31aa564f5ea95d408564ece
Elke Van Santvliet <elkevs96@live.be>
2024-12-16 18:26:26 +0100

    Completed day 15 part 2

--- day15/main.py
+++ day15/main.py
@@ -6,28 +6,41 @@ def main():
         map, movements = input.read().rstrip().split("\n\n")
     map = [list(row) for row in map.split("\n")]
     movements = movements.replace("\n", "")
-    map = simulate_movements(map, movements)
+    sim_map = simulate_movements(map, movements)
     print(
         sum(
             [
                 100 * i + j
-                for i in range(len(map))
-                for j in range(len(map[0]))
-                if map[i][j] == "O"
+                for i in range(len(sim_map))
+                for j in range(len(sim_map[0]))
+                if sim_map[i][j] == "O"
+            ]
+        )
+    )
+    sim_resized_map = simulate_movements(resize(map), movements)
+    print(
+        sum(
+            [
+                100 * i + j
+                for i in range(len(sim_resized_map))
+                for j in range(len(sim_resized_map[0]))
+                if sim_resized_map[i][j] == "["
             ]
         )
     )
 
 
 def simulate_movements(map: list[list[str]], movements: str) -> list[list[str]]:
-    for i, m in enumerate(movements):
-        map = attempt_move(map, m)
-    return map
+    sim_map = [row[:] for row in map]
+    for m in movements:
+        sim_map = attempt_move(sim_map, m)
+    return sim_map
 
 
 def attempt_move(
     map: list[list[str]], m: str, robot: tuple[int, int] | None = None
 ) -> list[list[str]]:
+    map = [row[:] for row in map]
     robot = (
         robot
         if robot
@@ -52,12 +65,47 @@ def attempt_move(
     if map[next[0]][next[1]] == ".":
         map[next[0]][next[1]] = map[ri][rj]
         map[ri][rj] = "."
-    elif map[next[0]][next[1]] == "O":
+    elif map[next[0]][next[1]] == "O" or (
+        map[next[0]][next[1]] in ("[", "]") and m in (">", "<")
+    ):
         map = attempt_move(map, m, next)
         if map[next[0]][next[1]] == ".":
             map[next[0]][next[1]] = map[ri][rj]
             map[ri][rj] = "."
+    elif map[next[0]][next[1]] == "[":
+        pot_map = attempt_move(map, m, next)
+        if pot_map[next[0]][next[1] + 1] == "]":
+            pot_map = attempt_move(pot_map, m, (next[0], next[1] + 1))
+        if pot_map[next[0]][next[1]] == "." and pot_map[next[0]][next[1] + 1] == ".":
+            map = pot_map
+            map[next[0]][next[1]] = map[ri][rj]
+            map[ri][rj] = "."
+    elif map[next[0]][next[1]] == "]":
+        pot_map = attempt_move(map, m, next)
+        if pot_map[next[0]][next[1] - 1] == "[":
+            pot_map = attempt_move(pot_map, m, (next[0], next[1] - 1))
+        if pot_map[next[0]][next[1]] == "." and pot_map[next[0]][next[1] - 1] == ".":
+            map = pot_map
+            map[next[0]][next[1]] = map[ri][rj]
+            map[ri][rj] = "."
     return map
+
+
+def resize(map: list[list[str]]) -> list[list[str]]:
+    new_map = [row[:] for row in map]
+    for i, row in enumerate(map):
+        for j, tile in enumerate(row):
+            match tile:
+                case "#":
+                    new_map[i].insert(2 * j, "#")
+                case "O":
+                    new_map[i][2 * j] = "]"
+                    new_map[i].insert(2 * j, "[")
+                case ".":
+                    new_map[i].insert(2 * j, ".")
+                case "@":
+                    new_map[i].insert(2 * j + 1, ".")
+    return new_map
 
 
 if __name__ == "__main__":
