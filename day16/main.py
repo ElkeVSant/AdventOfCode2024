0500a16138f9da6a0aa6f88e43ec60416f8a8fac
Elke Van Santvliet <elkevs96@live.be>
2024-12-19 11:17:52 +0100

    Completed day 16 part 2

--- day16/main.py
+++ day16/main.py
@@ -10,15 +10,15 @@ from typing import Self
 def main():
     with open("day16/input.txt") as map:
         maze = [line.rstrip() for line in map]
+    distances = dijkstra(maze)
     print(
         min(
-            [
-                dijkstra(maze)[i][row.index("E")]
-                for i, row in enumerate(maze)
-                if "E" in row
-            ][0].values()
+            [distances[i][row.index("E")] for i, row in enumerate(maze) if "E" in row][
+                0
+            ].values()
         )
     )
+    print(len([p for row in mark_paths(maze, distances) for p in row if p == "O"]))
 
 
 class Direction(Enum):
@@ -40,7 +40,7 @@ class Direction(Enum):
 
 @dataclass(order=True)
 class Node:
-    distance: int
+    distance: float
     position: tuple[int, int] = field(compare=False)
     direction: Direction = field(compare=False)
 
@@ -78,6 +78,8 @@ def dijkstra(
     distances[start.position[0]][start.position[1]][start.direction] = 0
     while len(to_visit):
         node = heappop(to_visit)
+        # if node.position == (7, 5):
+        #     print(node)
         if not math.isnan(
             distances[node.position[0] - 1][node.position[1]][Direction("N")]
         ) and distances[node.position[0] - 1][node.position[1]][
@@ -135,6 +137,52 @@ def dijkstra(
             )
             heappush(to_visit, west)
     return distances
+
+
+def mark_paths(
+    maze: list[str], distances: list[list[dict[Direction, float]]]
+) -> list[str]:
+    marked_maze = [row[:] for row in maze]
+    start = [(i, row.index("S")) for i, row in enumerate(maze) if "S" in row][0]
+    end = [(i, row.index("E")) for i, row in enumerate(maze) if "E" in row][0]
+    direction, distance = min(distances[end[0]][end[1]].items(), key=lambda x: x[1])
+    node = Node(distance, end, direction)
+    paths = []
+    heapify(paths)
+    heappush(paths, node)
+    marked_maze[node.position[0]] = (
+        marked_maze[node.position[0]][: node.position[1]]
+        + "O"
+        + marked_maze[node.position[0]][node.position[1] + 1 :]
+    )
+    while len(paths):
+        node = heappop(paths)
+        if node.position == start:
+            marked_maze[node.position[0]] = (
+                marked_maze[node.position[0]][: node.position[1]]
+                + "O"
+                + marked_maze[node.position[0]][node.position[1] + 1 :]
+            )
+            continue
+        match node.direction:
+            case Direction.N:
+                position = (node.position[0] + 1, node.position[1])
+            case Direction.E:
+                position = (node.position[0], node.position[1] - 1)
+            case Direction.S:
+                position = (node.position[0] - 1, node.position[1])
+            case _:
+                position = (node.position[0], node.position[1] + 1)
+        for dist in distances[position[0]][position[1]].items():
+            if dist[1] == node.distance - 1 - node.direction.diff(dist[0]):
+                previous_node = Node(dist[1], position, dist[0])
+                heappush(paths, previous_node)
+        marked_maze[node.position[0]] = (
+            marked_maze[node.position[0]][: node.position[1]]
+            + "O"
+            + marked_maze[node.position[0]][node.position[1] + 1 :]
+        )
+    return marked_maze
 
 
 if __name__ == "__main__":
