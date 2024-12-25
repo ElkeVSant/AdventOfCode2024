df44a2ed6c3d3c880742bedd872e9956b112f591
Elke Van Santvliet <elkevs96@live.be>
2024-12-25 14:30:50 +0100

    Completed day 19 part 2

--- day19/main.py
+++ day19/main.py
@@ -1,21 +1,34 @@
 #! /usr/bin/env python
 
+from functools import cache
+
 
 def main():
     with open("day19/input.txt") as brand_guide:
         patterns, designs = brand_guide.read().rstrip().split("\n\n")
-    patterns = patterns.split(", ")
+    patterns = tuple(patterns.split(", "))
     designs = designs.split("\n")
-    print(sum([compose_design(design, patterns) for design in designs]))
+    compositions = []
+    for design in designs:
+        compositions.append(compose_design(design, patterns))
+    # print(compositions)
+    print(sum([1 for comp in compositions if comp]))
+    print(sum(compositions))
 
 
-def compose_design(design: str, patterns: list[str]) -> bool:
+@cache
+def compose_design(
+    design: str,
+    patterns: tuple[str, ...],
+) -> int:
+    if not design:
+        return 1
+    compositions = 0
     for pattern in patterns:
         stripes = len(pattern)
         if design[:stripes] == pattern:
-            if stripes == len(design) or compose_design(design[stripes:], patterns):
-                return True
-    return False
+            compositions += compose_design(design[stripes:], patterns)
+    return compositions
 
 
 if __name__ == "__main__":
